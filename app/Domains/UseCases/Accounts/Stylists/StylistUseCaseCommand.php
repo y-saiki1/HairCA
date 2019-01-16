9415378eda1887f5c4fde4ea1fcf62393f1e3779
saiki yasunori <saiki.y1111@gmail.com>
2019-01-16 14:41:51 +0900

    [add] StylistUseCase

---
+++ app/Domains/UseCases/Accounts/Stylists/StylistUseCaseCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Domains\UseCases\Accounts\Stylists;
+
+use App\Domains\Models\BaseAccount\Account;
+use App\Domains\Models\Account\Stylist\Guest;
+
+interface StylistUseCaseCommand
+{
+    /**
+     * アカウント登録処理。パスワードを別にして渡している理由は、パスワードのハッシュ化をフレームワーク側に任せるため。
+     * フレームワーク側でログインの管理を行なっている場合、平文のパスワードを渡すとフレームワークがハッシュ化を行うため（laravelはそうなっている）、ドメイン層から外す。
+     * @param string アカウント名
+     * @param string メールアドレス
+     * @param string パスワード(平文)
+     * @return bool 保存成功
+     */
+    public function save(string $name, string $emailAddress, string $password): bool;
+
+    /**
+     * @param Guest ゲスト
+     * @return bool
+     */
+    public function saveGuest(Guest $guest): bool;
+}
---
+++ app/Domains/UseCases/Accounts/Stylists/StylistUseCaseQuery.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Domains\UseCases\Accounts\Stylists;
+
+use App\Domains\Exceptions\NotExistsException;
+use App\Domains\Exceptions\StylistProfileNotExistsException;
+
+use App\Domains\Models\BaseAccount\Account;
+use App\Domains\Models\Account\Stylist\Stylist;
+use App\Domains\Models\Account\Stylist\Guest;
+use App\Domains\Models\Hash;
+
+interface StylistUseCaseQuery
+{
+    /**
+     * メールアドレスと招待トークンでGuestユーザー（招待されたユーザー）を検索し、Guestを返す
+     * @param EmailAddress $email
+     * @param Hash 招待トークン
+     * @return Guest ゲスト
+     * @throws NotExistsException 指定メールアドレス・パスワードを持つゲストが存在しない時に発生する。
+     */
+    public function findGuestByEmailAddressAndToken(string $emailAddress, string $invitationToken): Guest;
+}
